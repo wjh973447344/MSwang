7909dc0de268dbc012d4bc25d1631bfb2446f083
wjh973447344 <54193487+wjh973447344@users.noreply.github.com>
2022-07-18 15:34:27 +0800

    Changes

--- src/main/java/com/wjh/mswang/MSwangApplication.java
+++ src/main/java/com/wjh/mswang/MSwangApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MSwangApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MSwangApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MSwangApplication.class, args);
+    }
 
 }
