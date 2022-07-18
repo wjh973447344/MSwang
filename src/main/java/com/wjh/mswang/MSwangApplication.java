71733c54476cfd4d348d9f0ab84c6e4d5d91fe97
wjh973447344 <54193487+wjh973447344@users.noreply.github.com>
2022-07-18 15:30:45 +0800

    Initial commit

---
+++ src/main/java/com/wjh/mswang/MSwangApplication.java
@@ -0,0 +1,13 @@
+package com.wjh.mswang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSwangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSwangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wjh/mswang/MSwangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjh.mswang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSwangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
