efd90956d1a84afdb912c3a39125403432efd1d0
wjh973447344 <54193487+wjh973447344@users.noreply.github.com>
2022-08-04 17:14:26 +0800

    增删改查

--- src/main/java/com/wjh/mswang/controller/userHandler.java
+++ src/main/java/com/wjh/mswang/controller/userHandler.java
@@ -1,22 +1,37 @@
 package com.wjh.mswang.controller;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.wjh.mswang.entity.user;
 import com.wjh.mswang.repository.userRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/")
 public class userHandler {
     @Autowired
     private userRepository UserRepository;
+
     @GetMapping("/findAll")
     public List<user> findAll() {
         return UserRepository.findAll();
     }
+
+    @GetMapping("/deleteById")
+    public void deleteById(Integer id) {
+        UserRepository.deleteById(id);
+    }
+    @PostMapping("/newOne")
+    public void newOne(@RequestBody user users) {
+        //user user = JSONObject.parseObject(users, user.class);
+        UserRepository.save(users);
+    }
+    @PostMapping("/flush")
+    public void flush(@RequestBody user users) {
+        UserRepository.saveAndFlush(users);
+    }
 }
+
 
